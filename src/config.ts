10047845a8dab085aca5551cf0d28d09da15020b
llnzjl <azizi@chungbuk.ac.kr>
2026-05-13 11:16:10 +0900

    fix certificate filenames - rename to correct .png extension

--- src/config.ts
+++ src/config.ts
@@ -47,12 +47,12 @@ export const siteConfig = {
       certificates: [
         {
           title: "Completion Certificate",
-          image: "/certificates/chungbuk-hackathon-completion.jpg.png",
+          image: "/certificates/chungbuk-hackathon-completion.png",
           note: "",
         },
         {
           title: "2nd Place Award Certificate",
-          image: "/certificates/chungbuk-hackathon-second-place.jpg.png",
+          image: "/certificates/chungbuk-hackathon-second-place.png",
           note: "",
         },
       ],
@@ -73,12 +73,12 @@ export const siteConfig = {
       certificates: [
         {
           title: "Completion Certificate",
-          image: "/certificates/chungbuk-hackathon-completion.jpg.png",
+          image: "/certificates/chungbuk-hackathon-completion.png",
           note: "",
         },
         {
           title: "2nd Place Award Certificate",
-          image: "/certificates/chungbuk-hackathon-second-place.jpg.png",
+          image: "/certificates/chungbuk-hackathon-second-place.png",
           note: "",
         },
       ],
